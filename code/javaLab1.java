21c1761e774505f6b7b1cf5518cc22717b4d8a84
siehun <126447514+siehun@users.noreply.github.com>
2023-10-12 10:44:53 +0800

    lab1

---
+++ code/javaLab1.java
@@ -0,0 +1,30 @@
+package new05;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class javaLab {
+    /**
+     *
+     * @author yuehaitao
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        LinkedList<Integer> li = new LinkedList<>();
+        for(int i = 1; i <= n; i++) {
+            li.add(i);
+        }
+        int time = sc.nextInt();
+        for(int i = 0; i < time; i++) {
+            int inte = sc.nextInt();
+            int index = li.indexOf(inte);
+            int step = sc.nextInt();
+            li.remove(index);
+            li.add(index+step,inte);
+        }
+        for(int ele: li) {
+            System.out.print(ele+" ");
+        }
+    }
+}
